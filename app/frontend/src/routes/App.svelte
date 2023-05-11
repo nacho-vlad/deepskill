7131b732e2f11ac766bc841be74908da19f589c8
Florin-Vlad Sabau <sabauflorin28@gmail.com>
2023-05-11 18:55:34 +0300

    Added frontend

---
+++ app/frontend/src/routes/App.svelte
@@ -0,0 +1,67 @@
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
+</svelte:head>
+
+
+<script>
+  import 'chota';
+</script>
+
+<div style="width:45%; margin:auto">
+
+  <h1>
+    DeepSkill
+  </h1>
+  <h2>
+    Rating Players with Temporal Graph Networks
+  </h2>
+  
+  <div style="">
+  <label for="White">White</label>
+  <input placeholder="Lichess Username" id="White">
+    
+  </div>
+  <div style="margin-bottom:20px">
+  <label for="Black">Black</label>
+  <input placeholder="Lichess Username" id="Black">
+  </div>
+  
+  <h2 style="margin-bottom:10px">
+    About
+  </h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  
+</div>
+
+
+<style>
+
+  * {
+    font-size: 1.1em;
+    font-family: "EB Garamond", sans-serif
+  }
+  
+  h1 {
+    font-size:5em; 
+    margin:0px; 
+    margin-top: 20px; 
+    padding: 0px; 
+    line-height:120%;
+    font-family: "EB Garamond", sans-serif
+  }
+  
+  h2 {
+    font-size:3em; 
+    width: 80%; 
+    margin:0px; 
+    margin-bottom: 40px;
+    padding:0px; 
+    line-height:120%;
+    font-family: "EB Garamond", sans-serif
+  }
+
+</style>
+
+
